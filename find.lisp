cc4785e6ba2d8feca7471839d7633dc3f4118a72
borutorinred <37692980+borutorinred@users.noreply.github.com>
2018-04-20 15:46:28 +0700

    Update find.lisp

--- find.lisp
+++ find.lisp
@@ -1,2 +1,8 @@
+(defun filter1 (pred list) 
+(cond ((null list) nil) 
+((funcall pred (car list)) 
+(cons (car list) 
+(filter1 pred (cdr list)))) 
+(T (filter1 pred (cdr list)))))
 (defun find-if1 (pred kurwa) 
 (car (filter1 pred kurwa)))
